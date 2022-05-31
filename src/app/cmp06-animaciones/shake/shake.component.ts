0c04f352719a10bed38d4d0a4c24d5063be899df
Francisco J. Hermoso <francisco.hermoso@sngular.com>
2022-05-31 18:15:31 +0200

    keyframe animation

--- src/app/cmp06-animaciones/shake/shake.component.ts
+++ src/app/cmp06-animaciones/shake/shake.component.ts
@@ -1,15 +1,54 @@
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shake',
   templateUrl: './shake.component.html',
-  styleUrls: ['./shake.component.css']
+  styleUrls: ['./shake.component.css'],
+  animations: [
+    trigger('shake', [
+      transition( 'no-shaking <=> shaking', [
+        animate(500, keyframes([
+          style({
+            transform: 'rotate(0deg)',
+            offset: 0
+          }),
+          style({
+            transform: 'rotate(-30deg)',
+            offset: 0.2
+          }),
+          style({
+            transform: 'rotate(30deg)',
+            offset: 0.4
+          }),
+          style({
+            transform: 'rotate(-30deg)',
+            offset: 0.6
+          }),
+          style({
+            transform: 'rotate(30deg)',
+            offset: 0.8
+          }),
+          style({
+            transform: 'rotate(0deg)',
+            offset: 1
+          })
+        ]))
+      ])
+    ])
+  ]
 })
 export class ShakeComponent implements OnInit {
+
+  shakeState: string = 'no-shaking';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarEstado() {
+    this.shakeState = (this.shakeState === 'no-shaking') ? 'shaking' : 'no-shaking';
   }
 
 }
